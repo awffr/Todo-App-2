98c5a15a5f846906c81663d90fff0127a76f723e
awffr <awfafarhan78@gmail.com>
2024-06-04 20:20:17 +0700

    Add: delete todo feature

--- Todo-App-2/src/App.jsx
+++ Todo-App-2/src/App.jsx
@@ -33,11 +33,16 @@ function App() {
     setTodos(updatedTodos)
   }
 
+  const deleteTodo = (todoId) => {
+    const updatedTodos = todos.filter(todo => todo.id !== todoId)
+    setTodos(updatedTodos)
+  }
+  
   return (
     <div style={styles.container}>
       <h1 style={styles.title}>My Todo List</h1>
       {/* Teruskan function toggleCompleted ke component Todos */}
-      <Todos todos={todos} toggleCompleted={toggleCompleted} />
+      <Todos todos={todos} toggleCompleted={toggleCompleted} deleteTodo={deleteTodo}/>
     </div>
   )
 }
